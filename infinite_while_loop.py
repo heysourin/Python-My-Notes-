e4aef6e666fd30a7d9bcf807aff51da723840be7
Sourin <98945276+heysourin@users.noreply.github.com>
2022-07-04 00:12:38 +0530

    Infinite input loop

--- infinite_while_loop.py
+++ infinite_while_loop.py
@@ -1,5 +1,5 @@
 while (len(name)) == 0:
     name = input("enter your name: ")
-    # ? this algo means it will run in infinite loop untill you enter name
+    #it will keep on asking your name if you submit it blank
 print("hello " + name)
 
